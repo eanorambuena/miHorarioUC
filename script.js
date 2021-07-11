a65a533c7f5d62bc99dcf55ed66dc603f5cf8d9d
eanorambuena <eanorambuena@uc.cl>
2021-07-11 13:58:09 -0400

    Add Ramo & Tabla classes

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 document.getElementById("title").innerHTML="Demo";
 document.getElementById("image").src="demo/HorarioUC.jpg";
+
+
+// class Tabla
+function Tabla() {
+    this.elementos=[]
+    this.display=function() {
+        document.write("");
+    };
+};
+
+// class Ramo
+function Ramo(nombre, link) {
+    this.nombre = nombre;
+    this.link = link;
+    this.add = function() {
+      document.write("");
+    };
+};
+
